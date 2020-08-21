1ebcdf263873c68b80c938f86ff6e896491937dc
Simon Galliker <sgalliker2@online.gibz.ch>
2020-08-21 14:39:39 +0200

    pasted header

---
+++ m403.first/m403.first.cpp
@@ -0,0 +1,129 @@
+/*******************************************************************************
+* Programm:
+* Filename:   .c
+*
+* Autor:      Simon Galliker
+* Version:    1.0
+* Datum:
+*
+* Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt, Zeit):
+* 1.0
+********************************************************************************
+*
+* Verwendungszweck: C-Schulung, M225+
+*
+* Beschreibung:
+*
+*
+* Precondition:  -
+*
+* Postcondition: -
+*
+* Benötigte Libraries:
+* - stdlib.h
+* - stdio.h
+*
+* Copyright (c) 2020 by S.Galliker, CH-6221 Rickenbach
+*******************************************************************************/
+
+/***  Include Files ***********************************************************/
+
+#include <iostream> /* Funktionsbibliothek: Standard Ein- Ausgabe */
+
+
+/***  Globale Deklarationen und Definitionen **********************************/
+
+
+/***  Funktions-Deklarationen *************************************************/
+
+
+
+/*******************************************************************************
+******************************* HAUPTPROGRAMM **********************************
+*******************************************************************************/
+int main(void) {
+
+    // lokale Variablen                                                                  
+
+
+    /* Intro --------------------- */
+
+
+    /* Eingabe ------------------- */
+
+
+    /* Verarbeitung -------------- */
+
+
+    /* Ausgabe ------------------- */
+
+
+    return (0);
+}
+
+
+
+/*******************************************************************************
+*************************** FUNKTIONS-DEFINITIONEN *****************************
+*******************************************************************************/
+
+/*******************************************************************************
+* Funktion:
+* Source: 	  .c
+* Call:
+*
+* Autor:     Simon Galliker
+* Version:   1.0
+* Datum:
+*
+* Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt):
+* 1.0
+********************************************************************************
+*
+* Verwendungszweck: C-Schulung, M225+
+*
+* Beschreibung:
+*
+*
+* Precondition: -
+*
+* Postcondition: -
+*
+* Benötigte Unterprogramme: -
+*
+* Parameter: (I: Input, O: Output, U: Update, R: Return)
+*
+*
+* Copyright (c) 2020 by S.Galliker, CH-6221 Rickenbach
+*******************************************************************************/
+
+
+
+
+
+
+
+
+// m403.first.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+#include <iostream> 
+
+int main() {
+
+    std::cout << "Hello World!";
+}
